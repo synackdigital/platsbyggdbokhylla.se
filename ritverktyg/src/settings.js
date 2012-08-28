99d7b08700405f99099aacedd3bd453988803699
Martin Lundin <martin@talkative.se>
2012-08-28 13:52:58 +0200

    yeah

---
+++ ritverktyg/src/settings.js
@@ -0,0 +1,138 @@
+var TEMPLATES = {
+	standard:[{
+		type:"single",
+		w:1800,
+		h:2400,
+		col:4,
+		row:4
+	}],
+	onewin:[
+		{
+			type:"std",
+			w:1800,
+			h:2400,
+			col:4,
+			row:4
+		},
+		{
+			type:"over",
+			w:1200,
+			h:1000,
+			col:2,
+			row:2
+		}
+	],
+	onewinmiddle:[
+		{
+			type:"std",
+			w:1800,
+			h:2400,
+			col:4,
+			row:4
+		},
+		{
+			type:"over",
+			w:1200,
+			h:1000,
+			col:2,
+			row:2
+		},
+		{
+			type:"std",
+			w:1800,
+			h:2400,
+			col:4,
+			row:4
+		}
+	],
+	threewin:[
+		{
+			type:"std",
+			w:1800,
+			h:2400,
+			col:4,
+			row:8
+		},
+		{
+			type:"over",
+			w:1200,
+			h:1000,
+			col:2,
+			row:2
+		},
+		{
+			type:"std",
+			w:1800,
+			h:2400,
+			col:4,
+			row:8
+		},
+		{
+			type:"over",
+			w:1200,
+			h:1000,
+			col:2,
+			row:2
+		},
+		{
+			type:"std",
+			w:1800,
+			h:2400,
+			col:4,
+			row:8
+		}
+	],
+};
+
+
+var PARTS = {
+	overlay:{minw:300,maxw:2010,minh:200,maxh:1000},
+	bottom:{h:22,b:60},
+	side:{w:22},
+	plane:{h:22,minamount:1},
+	kol:{w:22,minw:350,maxw:750},
+	dvd:{h:190,w:18,name:"DVD",image:"dvd"},
+	blueray:{h:172,w:16,name:"Blueray",image:"blueray"},
+	cd:{h:120,w:12,name:"CD-skiva",image:"cd"},
+	pocket:{h:180,w:30,name:"Pocket",image:"pocket"}
+};
+
+var STYLE_BLACK = {
+	bg:"135-#fff-#eee",
+	linecolor:"rgba(255,255,255,1)",
+	planefill:"0-#333-#666",
+	kolfill:"0-#333-#666",
+	sidefill:"0-#333-#666",
+	topfill:"0-#333-#666",
+	bottomfill:"0-#333-#666",
+	sockelfill:"90#333-#666"
+};
+var STYLE_RED = {
+	bg:"135-#fff-#eee",
+	linecolor:"rgba(255,200,200,1)",
+	planefill:"0-#a90329-#6d0019",
+	kolfill:"0-#a90329-#6d0019",
+	sidefill:"0-#a90329-#6d0019",
+	topfill:"0-#a90329-#6d0019",
+	bottomfill:"0-#a90329-#6d0019",
+	sockelfill:"90-#a90329-#6d0019",
+};
+var STYLE_BACK2 = {
+	bg:"135-#fff-#eee",
+	linecolor:"rgba(102,102,102,1)",
+	planefill:"90-#eee-#fff",
+	kolfill:"0-#eee-#fff",
+	sidefill:"0-#eee-#fff",
+	topfill:"90-#eee-#fff",
+	bottomfill:"90-#eee-#fff",
+};
+var STYLE = {
+	bg:"135-#fff-#eee",
+	linecolor:"rgba(30,30,30,1)",
+	planefill:"90-#F5F5F5-#fff",
+	kolfill:"0-#F5F5F5-#fff",
+	sidefill:"0-#F5F5F5-#fff",
+	topfill:"90-#F5F5F5-#fff",
+	bottomfill:"90-#F5F5F5-#fff",
+	sockelfill:"90#F5F5F5-#fff"
+};
