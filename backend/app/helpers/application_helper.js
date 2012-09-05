2c6d2dcf422a152bcc76736c0a7e56353d1c1529
Martin Lundin <martin@talkative.se>
2012-09-06 00:06:28 +0200

    Helper for link to platsbyggdbokhylla

--- backend/app/helpers/application_helper.js
+++ backend/app/helpers/application_helper.js
@@ -1,6 +1,10 @@
 var dateFormat = require('dateformat');
 
 module.exports = {
+	link_to_site: function(path) {
+		path = path ? path : "";
+		return "http://platsbyggdbokhylla.talkative.se" + path;
+	},
 	date_format: function (dateStr,format) {
 		var theDate = new Date(dateStr);
 		var format = format ? format : "yyyy-mm-dd HH:MM";
