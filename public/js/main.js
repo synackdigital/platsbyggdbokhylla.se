a5741752610be4881fe3dd0cfaa4b707593a2c68
frebro <frebro@gmail.com>
2013-01-27 16:26:00 +0100

    implemented avgrund and vimeo autoplay

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,16 @@
 $(function(){
+
+  // Auto strech background images
   var bgimgurl = $("#main").css('background-image');
   if (Modernizr.backgroundsize != true && bgimgurl != "none") {
     bgimgurl = bgimgurl.replace('url("','').replace('")','');
     $("#main").backstretch( bgimgurl );
   }
+
+  // Avgrund video modals
+  $('a.avgrund-link').click(function(){
+    $('#avgrund iframe').attr('src', $(this).attr('href'));
+    Avgrund.show( "#avgrund" );
+    return false;
+  })
 });
