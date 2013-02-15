b9ee647765a17b1ab5bb4a38e543bd955290fa55
Martin Lundin <martin@talkative.se>
2013-02-15 16:55:38 +0100

    yeah mannen

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,31 @@
-$(function(){
+if(window.Prototype!==undefined){
+  Event.observe(window,"load",function(){
+    // Avgrund video modals
+    $$('a.avgrund-link').each(function(aLink){
+      
+      aLink.observe("click",function(e){
+        e.stop();
+        $$('#avgrund iframe').first().writeAttribute('src', this.readAttribute('href'));
+        Avgrund.show( "#avgrund" );
+        return false;
+      });
+    })
+  });
+} else {
+  $(function(){
+    // Auto strech background images
+    var bgimgurl = $("#main").css('background-image');
+    if (Modernizr.backgroundsize != true && bgimgurl != "none") {
+      bgimgurl = bgimgurl.replace('url("','').replace('")','');
+      $("#main").backstretch( bgimgurl );
+    }
 
-  // Auto strech background images
-  var bgimgurl = $("#main").css('background-image');
-  if (Modernizr.backgroundsize != true && bgimgurl != "none") {
-    bgimgurl = bgimgurl.replace('url("','').replace('")','');
-    $("#main").backstretch( bgimgurl );
-  }
-
-  // Avgrund video modals
-  $('a.avgrund-link').click(function(){
-    $('#avgrund iframe').attr('src', $(this).attr('href'));
-    Avgrund.show( "#avgrund" );
-    return false;
-  })
-});
+    // Avgrund video modals
+    $('a.avgrund-link').click(function(e){
+      e.preventDefault();
+      $('#avgrund iframe').attr('src', $(this).attr('href'));
+      Avgrund.show( "#avgrund" );
+      return false;
+    })
+  });
+}
