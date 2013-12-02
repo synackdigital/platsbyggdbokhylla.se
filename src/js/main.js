da657c45df722b6656edfec6a0ace6989e8efab4
frebro <frebro@gmail.com>
2013-12-02 15:25:42 +0100

    responsive header

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,28 @@
+
 $(function(){
-  $('#slides').superslides({
-    play:                 '5200',
-    animation:            'fade',
-    inherit_width_from:   '#slides .slides-container',
-    inherit_height_from:  '#slides .slides-container'
-  });
+  console.log('$()');
+  app.init();
 });
+
+var app = {
+  el: {
+    slides:                 $('#slides'),
+    slidesContainer:        $('.slides-container', this.slides)
+  },
+
+  init: function() {
+    this.superslidesInit();
+  },
+
+  superslidesInit: function () {
+    console.log('superslidesInit()');
+    this.el.slides.superslides({
+      play:                 '5200',
+      animation:            'slide',
+      animation_easing:     'easeInOutCubic',
+      inherit_width_from:   this.el.slidesContainer,
+      inherit_height_from:  this.el.slidesContainer
+    });
+  },
+};
+
